ab42d25fe42e3f4620b5110c6c5cb13ba7bc3e78
jiayinghsu <jiayinghsu1@gmail.com>
2023-03-03 16:43:26 -0800

    correct code files

--- cascade_rcnn/leaf_segmentation_with_cascade_RCNN.py
+++ cascade_rcnn/leaf_segmentation_with_cascade_RCNN.py
@@ -1,71 +1,46 @@
-# Segment plant leaves using cascade R-CNN and calculate the area of each segmented instance. 
+'''
+Here, we first load the pretrained model 'cascade_rcnn_model.h5' and then load and preprocess the input image. 
+We then perform segmentation on the image using the model and extract the segmentation results, including the masks, class IDs, and scores.
+
+Next, we calculate the area of each segmented instance by finding the contour of the mask and using the cv2.contourArea function. 
+We store the areas in a list.
+
+Finally, we save the results as a CSV file using the pandas library. We create a dictionary of the class IDs, scores, and areas, and then convert it to a pandas DataFrame. 
+We then use the to_csv function to save the DataFrame as a CSV file.
+'''
 
 import cv2
 import numpy as np
-import tensorflow as tf
+import pandas as pd
+from keras.models import load_model
 
-# Load the trained model
-model = tf.keras.models.load_model('cascade_rcnn_model.h5')
+# Load the pretrained model
+model = load_model('cascade_rcnn_model.h5', compile=False)
 
-# Load the image to be segmented
-image = cv2.imread('plant_leaf_image.jpg')
-
-# Convert the image to RGB format
+# Load the image and preprocess it
+image = cv2.imread('plant_leaves.jpg')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-# Resize the image
 image = cv2.resize(image, (800, 800))
+image = image.astype(np.float32) / 255.0
 
-# Perform segmentation using the model
+# Perform segmentation on the image
 results = model.detect([image])
 
-# Get the number of instances segmented
-num_instances = len(results[0]['rois'])
+# Extract the segmentation results
+r = results[0]
+masks = r['masks']
+class_ids = r['class_ids']
+scores = r['scores']
 
-# Loop through the segmented instances and calculate their areas
-for i in range(num_instances):
-    # Get the bounding box coordinates for the instance
-    y1, x1, y2, x2 = results[0]['rois'][i]
-    # Crop the instance from the image
-    instance = image[y1:y2, x1:x2]
-    # Calculate the area of the instance
-    area = np.count_nonzero(cv2.cvtColor(instance, cv2.COLOR_RGB2GRAY))
-    # Print the area of the instance
-    print('Instance {}: Area = {} pixels'.format(i+1, area))
+# Calculate the area of each segmented instance
+areas = []
+for i in range(masks.shape[2]):
+    mask = masks[:, :, i]
+    contour = cv2.findContours(mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    area = cv2.contourArea(contour)
+    areas.append(area)
 
-
-
-
-##############
-
-# Load cascade R-CNN model for plant leaves segmentation
-model = cv2.dnn.readNetFromTensorflow('cascade_rcnn.pb')
-
-# Load image
-img = cv2.imread('plant_leaves.jpg')
-
-# Set threshold for detection
-confThreshold = 0.5
-
-# Run inference on the image using the cascade R-CNN model
-model.setInput(cv2.dnn.blobFromImage(img, size=(300, 300), swapRB=True, crop=False))
-output = model.forward()
-
-# Loop through all detected instances
-for i in range(output.shape[2]):
-    confidence = output[0, 0, i, 2]
-
-    # Only consider instances with confidence above the threshold
-    if confidence > confThreshold:
-        classID = int(output[0, 0, i, 1])
-        bbox = output[0, 0, i, 3:7] * np.array([img.shape[1], img.shape[0], img.shape[1], img.shape[0]])
-        bbox = bbox.astype(int)
-        cv2.rectangle(img, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 255, 0), 2)
-
-        # Calculate area of each instance
-        area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1])
-        print('Instance', i+1, 'Area:', area)
-
-# Save results as CSV file
-results = pd.DataFrame({'Instance': list(range(1, output.shape[2]+1)), 'Area': [int((bbox[2] - bbox[0]) * (bbox[3] - bbox[1])) for bbox in bboxes]})
-results.to_csv('plant_leaves_results.csv', index=False)
+# Save the results as a CSV file
+data = {'Class ID': class_ids, 'Score': scores, 'Area': areas}
+df = pd.DataFrame(data)
+df.to_csv('segmentation_results.csv', index=False)
--- cascade_rcnn/train_cascade_rcnn_model.py
+++ cascade_rcnn/train_cascade_rcnn_model.py
@@ -1,43 +1,68 @@
-'''
-Here, we first load the pretrained model 'cascade_rcnn_model.h5' and then load and preprocess the input image. We then perform segmentation on the image using the model and extract the segmentation results, including the masks, class IDs, and scores.
+# Train a cascade R-CNN model on your dataset of plant leaf images and save it as "cascade_rcnn_model.h5".
 
-Next, we calculate the area of each segmented instance by finding the contour of the mask and using the cv2.contourArea function. We store the areas in a list.
-
-Finally, we save the results as a CSV file using the pandas library. We create a dictionary of the class IDs, scores, and areas, and then convert it to a pandas DataFrame. We then use the to_csv function to save the DataFrame as a CSV file.
-'''
-
-import cv2
+import os
 import numpy as np
-import pandas as pd
-from keras.models import load_model
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.layers import Input
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.utils import plot_model
+from mrcnn.config import Config
+from mrcnn.model import MaskRCNN
+from mrcnn.utils import Dataset, compute_ap
 
-# Load the pretrained model
-model = load_model('cascade_rcnn_model.h5', compile=False)
+# Define the configuration for the model
+class PlantLeafConfig(Config):
+    NAME = "plant_leaf"
+    NUM_CLASSES = 1 + 1  # Background + plant leaf
+    STEPS_PER_EPOCH = 131
+    DETECTION_MIN_CONFIDENCE = 0.9
+    LEARNING_RATE = 0.001
 
-# Load the image and preprocess it
-image = cv2.imread('plant_leaves.jpg')
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-image = cv2.resize(image, (800, 800))
-image = image.astype(np.float32) / 255.0
 
-# Perform segmentation on the image
-results = model.detect([image])
+# Define the dataset class
+class PlantLeafDataset(Dataset):
+    def load_dataset(self, dataset_dir):
+        self.add_class("dataset", 1, "plant_leaf")
+        images_dir = os.path.join(dataset_dir, 'images')
+        annotations_dir = os.path.join(dataset_dir, 'annotations')
+        for filename in os.listdir(images_dir):
+            image_id = filename[:-4]
+            img_path = os.path.join(images_dir, filename)
+            ann_path = os.path.join(annotations_dir, image_id + '.txt')
+            self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path)
 
-# Extract the segmentation results
-r = results[0]
-masks = r['masks']
-class_ids = r['class_ids']
-scores = r['scores']
+    def load_mask(self, image_id):
+        info = self.image_info[image_id]
+        ann_path = info['annotation']
+        with open(ann_path, 'r') as f:
+            lines = f.readlines()
+        mask = np.zeros([info['height'], info['width'], len(lines)], dtype=np.uint8)
+        class_ids = np.zeros(len(lines), dtype=np.int32)
+        for i, line in enumerate(lines):
+            class_id, coords = line.split()
+            class_ids[i] = 1  # Only one class (plant leaf)
+            coords = list(map(int, coords.split(',')))
+            x1, y1, x2, y2 = coords
+            mask[y1:y2, x1:x2, i] = 1
+        return mask, class_ids
 
-# Calculate the area of each segmented instance
-areas = []
-for i in range(masks.shape[2]):
-    mask = masks[:, :, i]
-    contour = cv2.findContours(mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-    area = cv2.contourArea(contour)
-    areas.append(area)
 
-# Save the results as a CSV file
-data = {'Class ID': class_ids, 'Score': scores, 'Area': areas}
-df = pd.DataFrame(data)
-df.to_csv('segmentation_results.csv', index=False)
+# Load the dataset
+dataset = PlantLeafDataset()
+dataset.load_dataset('path/to/your/dataset')
+dataset.prepare()
+
+# Load the ResNet50 model
+model = ResNet50(include_top=False, input_shape=(256, 256, 3))
+
+# Load the Mask R-CNN model with cascade classifier
+rcnn_model = MaskRCNN(mode='training', model_dir='./', config=PlantLeafConfig())
+
+# Train the model
+rcnn_model.train(dataset, model, epochs=20, layers='heads', learning_rate=PlantLeafConfig.LEARNING_RATE)
+
+# Save the trained model
+rcnn_model.keras_model.save('cascade_rcnn_model.h5')
