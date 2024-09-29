63a63da4c189e2a6904b49e68a3d25b8113e8e02
jiayinghsu <jiayinghsu1@gmail.com>
2024-09-29 11:00:32 -0700

    wip

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,13 +1,15 @@
+# test_package.py
+
 from plant_growth_tracker import process_images
 
-# Path to a folder containing test images
-image_folder = 'data'
+# Replace with the actual path to your image folder
+image_folder_path = 'data'
 
-# Process images using the package
+# Process images
 df = process_images(
-    image_folder_path=image_folder,
-    output_csv='test_results.csv',
+    image_folder_path=image_folder_path,
     segmentation_type='total_plant_area'  # or 'individual_leaf_area'
 )
 
+# Print the results
 print(df)
